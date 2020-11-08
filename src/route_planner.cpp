d50527ef99aa009ededcf64085806fde6e593b63
Lance Smith <artieTwelve@gmail.com>
2020-11-08 19:01:41 +0000

    Fixes

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -11,6 +11,10 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     // TODO 2: Use the m_Model.FindClosestNode method to find the closest nodes to the starting and ending coordinates.
     // Store the nodes you find in the RoutePlanner's start_node and end_node attributes.
 
+    this->start_node = &m_Model.FindClosestNode(start_x,start_y);
+    this->end_node = &m_Model.FindClosestNode(end_x,end_y);
+
+
 }
 
 
@@ -21,6 +25,12 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
 
+    // explict input floats for clarity. The compiler will optimize all this away.
+    float current_x = node->x;
+    float current_y = node->y;
+    float end_x = end_node->x;
+    float end_y = end_node->y;
+    return std::sqrt(std::pow((current_x - end_x), 2) + std::pow((current_y - end_y), 2));
 }
 
 
@@ -33,6 +43,15 @@ float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
 
 void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
 
+    // Find all neighbors of the current node, set the values and push onto the open list
+    current_node->FindNeighbors();
+    for(auto neighbor: current_node->neighbors) {
+        neighbor->parent = current_node;
+        neighbor->h_value = CalculateHValue(neighbor);
+        neighbor->g_value = current_node->g_value + current_node->distance(*neighbor);
+        neighbor->visited = true;
+        open_list.push_back(neighbor);
+    }
 }
 
 
@@ -45,8 +64,19 @@ void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
 
 RouteModel::Node *RoutePlanner::NextNode() {
 
-}
+ // Sort the nodes in descending order. Node with smallest distance will be last.
+ std::sort(open_list.begin(), open_list.end(), [](RouteModel::Node* a, RouteModel::Node* b) {
+        float z = a->g_value + a->h_value;
+        float x = b->g_value + b->h_value;
+        return z > x;   
+    });
+    // Remove the node with the last node from the open list and return a pointer to it.
+    RouteModel::Node* leastDistance = open_list.back();
+    open_list.pop_back();
 
+    return leastDistance;
+
+}
 
 // TODO 6: Complete the ConstructFinalPath method to return the final path found from your A* search.
 // Tips:
@@ -62,7 +92,15 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
     std::vector<RouteModel::Node> path_found;
 
     // TODO: Implement your solution here.
-
+    // Calculate the path distance
+    while((current_node->x != start_node->x) && (current_node->y != start_node->y)) {
+        RouteModel::Node *parent = current_node->parent;
+        distance += current_node->distance((*parent));
+        path_found.push_back((*current_node));
+        current_node = current_node->parent;
+    } 
+    path_found.push_back((*current_node));
+    std::reverse(std::begin(path_found),std::end(path_found));
     distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
     return path_found;
 
@@ -78,7 +116,23 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
 
 void RoutePlanner::AStarSearch() {
     RouteModel::Node *current_node = nullptr;
+   
+    // While the open list is not empty and the start node does not equal the end node
+    start_node->visited = true;
+    current_node = start_node;
+    open_list.push_back(current_node);
 
-    // TODO: Implement your solution here.
+       while(!open_list.empty()) { // if list is empty then no route is found
+          
+            current_node = NextNode();
+
+            if(current_node->x == end_node->x && current_node->y == end_node->y) {
+                // if start == end then a route has been found
+                break;
+            }
+             AddNeighbors(current_node);
+        }
+  
+    m_Model.path = ConstructFinalPath(current_node);
 
 }
